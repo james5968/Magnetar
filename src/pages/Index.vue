9866dce90964f3cc25ef74e90c2de965b005bdf8
James Ashford <james.ashford@hotmail.com>
2020-05-31 15:34:49 +0100

    State retention

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,34 +1,111 @@
 <template>
-  <q-page>
-    <div class="row justify-center">
-      <clock class="clock-holder "></clock>
-    </div>
-    <div class="row weather-comp justify-center ">
-      <weather />
-    </div>
+  <q-page v-touch-hold:1000.mouse="handleHold">
+    <editBar
+      @close-edit="closeEdit()"
+      @save-edit="closeEdit()"
+      @close-check="endTransform()"
+      :editMode="editMode"
+      :transformMode="transformMode"
+    />
+    <widgetFrame
+      
+      widgetWidth="420"
+      widgetHeight="420"
+      :transformOn="transformMode"
+      :editMode="editMode"
+      id="analogClock"
+    >
+      <analogClock slot="widget" />
+    </widgetFrame>
+    <widgetFrame
+      widgetWidth="300"
+      widgetHeight="100"
+      :transformOn="transformMode"
+      :editMode="editMode"
+      id="digitalClock"
+    >
+      <DigitalClock
+        slot="widget"
+        style="font-size:100px; line-height:65px"
+        :blink="true"
+      />
+    </widgetFrame>
+    <widgetFrame
+      widgetWidth="630"
+      widgetHeight="230"
+      :transformOn="transformMode"
+      :editMode="editMode"
+      id="weather"
+    >
+      <weather slot="widget" style="width:600px; height:200px" />
+      <weatherSettings slot="widget-settings"  />
+    </widgetFrame>
+    <widgetFrame
+      widgetWidth="480"
+      widgetHeight="150"
+      :transformOn="transformMode"
+      :editMode="editMode"
+      id="currency"
+    >
+      <currency slot="widget" style="width:450px; height:100px" />
+    </widgetFrame>
+
+    <div
+      v-if="!editMode"
+      class="swipe-box"
+      v-touch-swipe.mouse.left="handleSwipeUp"
+    ></div>
   </q-page>
 </template>
 
 <script>
-import clock from "../components/mainClock.vue";
+import widgetFrame from "../components/frames/widgetFrame.vue";
+import analogClock from "../components/mainClock.vue";
+import DigitalClock from "vue-digital-clock";
+import editBar from "../components/UI/editBar.vue";
 import weather from "../components/weather.vue";
+import currency from "../components/currency.vue";
+import weatherSettings from "../components/weatherSettings.vue";
 export default {
   components: {
-    clock,
-    weather
+    widgetFrame,
+    analogClock,
+    DigitalClock,
+    editBar,
+    weather,
+    currency,
+    weatherSettings
+  },
+  data() {
+    return {
+      editMode: false,
+      transformMode: false,
+      activeWidgets:{}
+    };
+  },
+  methods: {
+    handleHold() {
+      this.transformMode = !this.transformMode;
+    },
+    handleSwipeUp() {
+      this.editMode = true;
+    },
+    endTransform() {
+      this.transformMode = false;
+    },
+    closeEdit() {
+      this.editMode = false;
+    }
   }
 };
 </script>
 
 <style>
-.clock-holder {
-  max-width: 90vmin !important;
-  max-height: 90vmin !important;
-  width: 45vw;
-  transform: scale(0.7);
-}
-.weather-comp {
-  margin-top: 300px;
-  transform: scale(0.5);
+.swipe-box {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 30vw;
+  height: 100vh;
 }
 </style>
